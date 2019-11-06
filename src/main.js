f5ebcd4fa147d931c1523204b63ff89eb7970b62
zhou <2807356520@qq.com>
2019-11-06 18:52:48 +0800

    文件纠错

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import router from './router/index'
 import store from './store/index'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import Vant from 'vant';
-import 'vant/lib/index.css';
 
-Vue.use(Vant)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
